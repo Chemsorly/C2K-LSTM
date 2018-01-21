fb293dfdf9155e0ad2a5d8a227015fed39698c4f
Chemsorly <contact@chemsorly.com>
2018-01-21 15:40:05 +0100

    move result files back to default naming

--- code/aio_numeric_s2e_reversed.py
+++ code/aio_numeric_s2e_reversed.py
@@ -408,14 +408,14 @@ elif par_algorithm == 7:
 
 model.compile(loss={'act_output':'categorical_crossentropy', 'time_output':'mae'}, optimizer=opt)
 early_stopping = EarlyStopping(monitor='val_loss', patience=par_patience)
-model_checkpoint = ModelCheckpoint('output_files/models/model-latest-{}.h5'.format(filename), monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='auto')
+model_checkpoint = ModelCheckpoint('output_files/models/model-latest.h5', monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='auto')
 lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=par_patience, verbose=0, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
 
 #train
 model.fit(X, {'act_output':y_a, 'time_output':y_t}, validation_split=0.2, verbose=2, callbacks=[early_stopping, model_checkpoint, lr_reducer], batch_size=maxlen, nb_epoch=500)
 
 #prediction:
-model = load_model('output_files/models/model-latest-{}.h5'.format(filename))
+model = load_model('output_files/models/model-latest.h5')
 
 lines = fold3
 lines_t = fold3_t
@@ -453,7 +453,7 @@ def getSymbolPrediction(predictions):
         i += 1
     return symbol
 
-with open('output_files/results/results-{}.csv'.format(filename), 'wb') as csvfile:
+with open('output_files/results/results.csv', 'wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     spamwriter.writerow(["sequenceid","sequencelength", "prefix", "sumprevious", "timestamp", "completion", "gt_sumprevious", "gt_timestamp", "gt_planned", "gt_instance", "prefix_activities", "predicted_activities"])
     sequenceid = 0
